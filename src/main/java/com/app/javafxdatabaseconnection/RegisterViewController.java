7baeedb4d99c05b39bb892778046df6831874984
Jose Luis <condegomesjoseluis@gmail.com>
2024-05-18 18:44:28 +0200

    Add the id at the components.

--- src/main/java/com/app/javafxdatabaseconnection/RegisterViewController.java
+++ src/main/java/com/app/javafxdatabaseconnection/RegisterViewController.java
@@ -1,6 +1,17 @@
 package com.app.javafxdatabaseconnection;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
 public class RegisterViewController {
 
-
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private TextField stateField;
+    @FXML
+    private ChoiceBox rolField;
 }
