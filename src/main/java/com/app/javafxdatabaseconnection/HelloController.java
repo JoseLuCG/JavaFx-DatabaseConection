bc68b233bd758529bc3fa454ac8b14c40406a1bb
Jose Luis <condegomesjoseluis@gmail.com>
2024-05-18 18:31:34 +0200

    Add a new view.

--- src/main/java/com/app/javafxdatabaseconnection/HelloController.java
+++ src/main/java/com/app/javafxdatabaseconnection/HelloController.java
@@ -1,14 +1,29 @@
 package com.app.javafxdatabaseconnection;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
+    private TextField emailField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private Label welcomeText1;
+    @FXML
     private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void singInController(ActionEvent actionEvent) {
+        String email = emailField.getText();
+        String newText = email + "Pelele";
+        emailField.setText(newText);
+
+    }
+
+    public void newAcountController(ActionEvent actionEvent) {
+        System.out.println("adios");
+
     }
 }
---
+++ src/main/java/com/app/javafxdatabaseconnection/RegisterViewController.java
@@ -0,0 +1,6 @@
+package com.app.javafxdatabaseconnection;
+
+public class RegisterViewController {
+
+
+}
