bfb9dbec3fdb6ddd40b4319648247dc4542af29f
Jose Luis <joseluisconde@protonmail.com>
2024-05-21 10:29:00 +0200

    Creates a file that saves the methods that will start each of the windows.

--- src/main/java/com/dependences/Tools/Views.java
+++ src/main/java/com/dependences/Tools/Views.java
@@ -1,2 +1,25 @@
-package com.dependences.Tools;public class Views {
+package com.dependences.Tools;
+
+import com.app.javafxdatabaseconnection.HelloApplication;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Views {
+
+    public static void mainView () {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
+            Parent mainRoot = fxmlLoader.load();
+            Stage mainStage = new Stage();
+            mainStage.setTitle("Ventana Principal");
+            mainStage.setScene(new Scene(mainRoot,1500,900));
+            mainStage.show();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
