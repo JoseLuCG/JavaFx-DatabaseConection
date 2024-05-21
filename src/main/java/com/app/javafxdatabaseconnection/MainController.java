6b100576814074a85c14976ea8632ecd2fded7cd
Jose Luis <joseluisconde@protonmail.com>
2024-05-21 11:17:02 +0200

    Creates the main-view references.

--- src/main/java/com/app/javafxdatabaseconnection/MainController.java
+++ src/main/java/com/app/javafxdatabaseconnection/MainController.java
@@ -1,9 +1,47 @@
 package com.app.javafxdatabaseconnection;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class MainController {
     @FXML
-    private Label labelId;
+    private TableView usersTable;
+    @FXML
+    private TableColumn tableEmailField;
+    @FXML
+    private TableColumn tablePasswordField;
+    @FXML
+    private TableColumn tableStateField;
+    @FXML
+    private TableColumn tableRolField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private TextField stateField;
+    @FXML
+    private ChoiceBox rolField;
+    @FXML
+    private Button addUserButton;
+    @FXML
+    private Button updateUserButton;
+    @FXML
+    private Button deleteUSerButton;
+    @FXML
+    private Button loadUsersButton;
+
+    public void loadData(ActionEvent actionEvent) {
+        System.out.println("works");
+    }
+
+    public void AddUser(ActionEvent actionEvent) {
+    }
+
+    public void updateUser(ActionEvent actionEvent) {
+    }
+
+    public void deleteUser(ActionEvent actionEvent) {
+    }
 }
