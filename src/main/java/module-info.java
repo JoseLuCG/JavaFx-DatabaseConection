e474c5f38a08ad4fe46df8da11dc1461171eb45c
Jose Luis <condegomesjoseluis@gmail.com>
2024-05-23 01:49:53 +0200

    Fixes the error that 'javafx.base' cannot access the properties of the User class.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,10 +5,12 @@ module com.app.javafxdatabaseconnection {
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
     requires java.sql;
+    requires java.base;
 
     opens com.app.javafxdatabaseconnection to javafx.fxml;
     opens com.dependences.Database to java.base;
-    opens com.dependences.Models to java.base;
+    //opens com.dependences.Models to java.base;
+    opens com.dependences.Models to javafx.base;
     opens com.dependences.Tools to java.base;
     exports com.app.javafxdatabaseconnection;
 }
